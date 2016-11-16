7ab57d61e5e177de702109798ee8073d0f581bab
liangxu <liangxuav@gmail.com>
2016-11-16 10:24:51 -0500

    code final 2

---
+++ code_Final2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+java BGSeparation imgs/bear_small_input.png imgs/bear_small_inputFG.png imgs/bear_small_inputBG.png output/bear_fgOut.png output/bear_bgOut.png
+java BGSeparation imgs/grain_small_input.png imgs/grain_small_inputFG.png imgs/grain_small_inputBG.png output/grain_fgOut.png output/grain_bgOut.png
+java BGSeparation imgs/ski_small_input.png imgs/ski_small_inputFG.png imgs/ski_small_inputBG.png output/ski_fgOut.png output/ski_bgOut.png
+java BGSeparation imgs/tigerNoise_small_input.png imgs/tigerNoise_small_inputFG.png imgs/tigerNoise_small_inputBG.png output/tiger_fgOut.png output/tiger_bgOut.png
+java BGSeparation imgs/vegNoise_small_input.png imgs/vegNoise_small_inputFG.png imgs/vegNoise_small_inputBG.png output/vegNoise_fgOut.png output/vegNoise_bgOut.png
+
