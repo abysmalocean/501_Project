6109bcb3bd60844b08a40f67628a24cada54ad33
liangxu <liangxuav@gmail.com>
2016-11-16 10:11:20 -0500

    final code

---
+++ code_Final/adjacentlsit.java
@@ -0,0 +1,13 @@
+
+public class adjacentlsit {
+	public int x;
+	public int y;
+	public int id;
+	public adjacentlsit(int x, int y, int id)
+	{
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+
+}
