120400bbf8e71119ab12300c0d0ad94b145e0957
liangxu <liangxuav@gmail.com>
2016-11-16 10:10:00 -0500

    code 4 modify

--- code4/run.sh
+++ code4/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-javac BGSeparation
 java BGSeparation imgs/bear_small_input.png imgs/bear_small_inputFG.png imgs/bear_small_inputBG.png output/bear_fgOut.png output/bear_bgOut.png
 java BGSeparation imgs/grain_small_input.png imgs/grain_small_inputFG.png imgs/grain_small_inputBG.png output/grain_fgOut.png output/grain_bgOut.png
 java BGSeparation imgs/ski_small_input.png imgs/ski_small_inputFG.png imgs/ski_small_inputBG.png output/ski_fgOut.png output/ski_bgOut.png
